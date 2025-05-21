e94a109c4d88bcb7eb9acbc0f9bf45ab995c9b1b
Tabarcraft <166629849+HautotQ@users.noreply.github.com>
2025-05-21 19:15:55 +0200

    Update and rename Rename_This_File.js to Rename.js

---
+++ AIPlayer/BP/scripts/FindOres/Rename.js
@@ -0,0 +1,12 @@
+import { world, BlockLocation, MinecraftBlockTypes, system } from "@minecraft/server";
+
+function sortCommand(command) {
+  switch (ev.message) {
+  case "/start":
+    // lancer IA
+    break;
+  case "/scan":
+    // lancer la fonction de scan
+    break;
+  }
+}

e46bf45664a3b09e0c1475a37eadce37f580f85b
Tabarcraft <166629849+HautotQ@users.noreply.github.com>
2025-05-21 19:21:09 +0200

    Update Rename.js

--- AIPlayer/BP/scripts/FindOres/Rename.js
+++ AIPlayer/BP/scripts/FindOres/Rename.js
@@ -3,7 +3,14 @@ import { world, BlockLocation, MinecraftBlockTypes, system } from "@minecraft/se
 function sortCommand(command) {
   switch (ev.message) {
   case "/start":
-    // lancer IA
+      world.events.playerJoin.subscribe(event => {
+        const player = event.player;
+        const entityType = "tabarcraft:ai_agent"; // Remplace par le type réel de ton entité
+
+        const ia = genererIAAutourDuJoueur(player, entityType);
+
+        player.runCommand(`tellraw @a {"rawtext":[{"text":"[IA] Humanoïde générée près de ${player.name} !"}]}`);
+      });
     break;
   case "/scan":
     // lancer la fonction de scan
